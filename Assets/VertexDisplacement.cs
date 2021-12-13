06d7b5a1bbd58469dfb0e7f35bf3ed673359cf45
bapek <67985782+bapek@users.noreply.github.com>
2021-12-13 16:53:37 +0300

    New Cave Environment

--- Assets/VertexDisplacement.cs
+++ Assets/VertexDisplacement.cs
@@ -23,7 +23,7 @@ public class VertexDisplacement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("Hand"))
         {
