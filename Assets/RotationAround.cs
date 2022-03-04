afec41af7a6e1183e8a0cb76851a1adcfdfde0cf
Warja Rybakova <s0562770@htw-berlin.de>
2022-03-04 12:15:57 +0100

    all buttons integrated

--- Assets/RotationAround.cs
+++ Assets/RotationAround.cs
@@ -8,7 +8,7 @@ public class RotationAround : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(ShowAndHide(button, 5.0f));
+        StartCoroutine(ShowAndHide(button, 7.0f));
     }
 
     // Update is called once per frame
